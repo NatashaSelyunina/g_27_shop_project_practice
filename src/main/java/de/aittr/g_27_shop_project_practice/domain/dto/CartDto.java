41aa56c0152bb1ffd83596b6f735c510e66679dc
Natalya Selyunina <seluninanv@mail.ru>
2024-02-19 14:19:19 +0300

    feat : class CartDto

--- src/main/java/de/aittr/g_27_shop_project_practice/domain/dto/CartDto.java
+++ src/main/java/de/aittr/g_27_shop_project_practice/domain/dto/CartDto.java
@@ -1,4 +1,46 @@
 package de.aittr.g_27_shop_project_practice.domain.dto;
 
+import de.aittr.g_27_shop_project_practice.domain.interfaces.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CartDto {
+    private int id;
+    private List<Product> products = new ArrayList<>();
+
+    public CartDto(int id, List<Product> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartDto cartDto = (CartDto) o;
+        return id == cartDto.id && Objects.equals(products, cartDto.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, products);
+    }
+
+    @Override
+    public String toString() {
+        return "CartDto{" +
+                "id=" + id +
+                ", products=" + products +
+                '}';
+    }
 }
